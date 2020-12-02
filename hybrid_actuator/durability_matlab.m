461c3266d9538784b92070d025dc35374c426128
Marisa Lim <65386928+marisa-lim@users.noreply.github.com>
2020-12-02 23:24:54 +0800

    clean up code

--- hybrid_actuator/durability_matlab.m
+++ hybrid_actuator/durability_matlab.m
@@ -1,18 +1,12 @@
 % MATLAB Code to find peaks for every 5000 cycles of durability test
-% Written by Marisa Lim
-
-% Loading file - opens a window for user to select .txt file
+% Written by Marisa Lim (marisa.lim@u.nus.edu)
+% .txt files from sensor required, delimiter: space
 
 clc
 clear
 
-%[name, path] = uigetfile('*.txt', 'Select file to open');
-% 
-%file = [path, name];
-
 % Load data
-%data = load(file);
-%csvwrite(file, data); % To remove any headerlines and get the first column
+
 filename1 = '_nrp finger 42113 cycles.txt';
 fileID1 = fopen(filename1);
 data1 = textscan(fileID1, '%f %f');
@@ -24,24 +18,24 @@ filename2 = '_nrp finger 11137 cycles_.txt';
 fileID2 = fopen(filename2);
 data2 = textscan(fileID2, '%f %f');
 fclose(fileID2);
-x2(:,1) = data2{:,1}+42113+1;% c: cycle vector
-x2(:,2) = data2{:,2};
+x2(:,1) = data2{:,1}+42113+1;% column 1: cycle vector
+x2(:,2) = data2{:,2};% column 2: pressure data in kPa
 
 filename3 = '_nrp finger 14959 cycles_.txt';
 fileID3 = fopen(filename3);
 data3 = textscan(fileID3, '%f %f');
 fclose(fileID3);
-x3(:,1) = data3{:,1}+42113+11137+2; % c: cycle vector
-x3(:,2) = data3{:,2};% x: pressure data in kPa
+x3(:,1) = data3{:,1}+42113+11137+2; % column 1: cycle vector
+x3(:,2) = data3{:,2};% column 2: pressure data in kPa
 
 filename4 = '_nrp finger 3103 cycles_.txt';
 fileID4 = fopen(filename4);
 data4 = textscan(fileID4, '%f %f');
 fclose(fileID4);
-x4(:,1) = data4{:,1}+42113+11137+14959+3; % c: cycle vector
-x4(:,2) = data4{:,2};% x: pressure data in kPa
+x4(:,1) = data4{:,1}+42113+11137+14959+3; % column 1: cycle vector
+x4(:,2) = data4{:,2};% column 2: pressure data in kPa
 
-x = cat(1, x1, x2, x3, x4);
+x = cat(1, x1, x2, x3, x4); %concatenate individual arrays
 
 last_cycle = x(end,1);
 n_cycles = fix(last_cycle/5000) + 1; % number of cycles we are interested in (floor division)
